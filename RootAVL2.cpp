0806f3718bdf33b72b1ddb6d952bd5cb5fd56fb5
namesfei <guofei.gf@outlook.com>
2020-04-26 00:01:30 +0800

    avltreeInsert 的递归函数

---
+++ RootAVL2.cpp
@@ -0,0 +1,17 @@
+//#include"AVLTreeInsert.h"
+//using namespace std;
+////µÝ¹éÊµÏÖ
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	AVLTree A=NULL;
+//	for (int i = 0; i < N; i++)
+//	{
+//		int a;
+//		cin >> a;
+//		A = Insert(A,a);
+//	}
+//	if(A)
+//		cout << A->Data;
+//}
